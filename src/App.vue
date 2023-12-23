03b32b33de5c60ac78bd751f2b140dadb0924853
EndoHizumi <endohizumi0114@gmail.com>
2023-12-24 05:21:30 +0900

    add: 寿司ｶﾞ飛ぶ

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,83 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="header">
+    <h1>sushilyRain</h1>
+    <h2>The rain will turn into sushi after midnight...</h2>
+    <div>
+      <label>sushi:</label>
+      <button @click="addSushi">Add</button>
+    </div>
+    <div>
+      <label>autoAdd:</label>
+      <button @click="isAutoAdd = !isAutoAdd">{{ isAutoAdd ? 'stop' : 'start' }}</button>
+    </div>
+  </div>
+  <div>
+    <div v-for="rain in rains" :key="rain.id" class="box" :class="`box${rain.id}`" :style="rain.style"
+      @animationend="rain.show = false" v-show="rain.show">
+      🍣
+    </div>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import { ref, watch } from 'vue';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+let nextId = 0;
+let intervalId = null;
+const isAutoAdd = ref(true);
+const rains = ref([]);
+const addSushi = () => {
+  const randomLeft = Math.random() * (window.innerWidth - 50); // 50はボックスの幅
+  const newItem = {
+    id: nextId++,
+    show: true,
+    style: {
+      left: `${randomLeft}px`,
+      animation: `moveDiagonally ${Math.random() * 10}s forwards`
+    }
+  };
+  rains.value.push(newItem);
+}
+intervalId = setInterval(addSushi, 800)
+const manageAutoAdd = () => {
+  if (isAutoAdd.value) {
+    intervalId = setInterval(addSushi, 800)
+  } else {
+    clearInterval(intervalId);
   }
 }
+
+watch(isAutoAdd, manageAutoAdd);
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+button {
+  margin-left: 5px;
+}
+
+.box {
+  position: absolute;
+  top: 0px;
+  z-index: 1;
+  font-size: 50px;
+}
+
+.header {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  z-index: 999;
+  position: relative;
+}
+
+body {
+  background-color: #121212;
+  color: rgb(205, 204, 204);
+  overflow: hidden;
+}
+
+@keyframes moveDiagonally {
+
+  to {
+    transform: translate(calc(100vw - -50px), calc(100vh - 50px)) rotate(300deg);
+  }
 }
 </style>
